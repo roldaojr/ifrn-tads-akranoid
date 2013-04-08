2e31edacf9adb89cb001f72449185765aee281a6
Roldão Rego Jr <roldaogjr@gmail.com>
2013-04-07 21:37:48 -0300

    Bola rebatendo

--- arkanoid/src/br/ifrn/tads/arkanoid/jogo/Bola.java
+++ arkanoid/src/br/ifrn/tads/arkanoid/jogo/Bola.java
@@ -5,11 +5,17 @@ import java.awt.Graphics2D;
 
 public class Bola extends ElementoDaTela {
 
+    private CenaDeJogo cena;
+    private int velocidadeX;
+    private int velocidadeY;
     protected int direcao;
     protected int velocidade;
 
-    public Bola(int i, int i1) {
+    public Bola(CenaDeJogo c, int i, int i1) {
         super(i, i1, 16, 16);
+        cena = c;
+        velocidadeX = 10;
+        velocidadeY = -10;
     }
 
     public int getDirecao() {
@@ -28,6 +34,25 @@ public class Bola extends ElementoDaTela {
         this.velocidade = velocidade;
     }
 
+    public void Mover() {
+        x += velocidadeX;
+        y += velocidadeY;
+        if(x < 0) {
+            x = 0;
+            velocidadeX = -velocidadeX;
+        } else if(x > (cena.getWidth() - width)) {
+            x = cena.getWidth() - width;
+            velocidadeX = -velocidadeX;
+        }
+        if(y < 0) {
+            y = 0;
+            velocidadeY = -velocidadeY;
+        } else if(y > (cena.getHeight() - height)) {
+            y = cena.getHeight() - height;
+            velocidadeY = -velocidadeY;
+        }
+    }
+    
     @Override
     public void Paint(Graphics2D g) {
         g.setColor(Color.RED);
--- arkanoid/src/br/ifrn/tads/arkanoid/jogo/CenaDeJogo.java
+++ arkanoid/src/br/ifrn/tads/arkanoid/jogo/CenaDeJogo.java
@@ -1,29 +1,37 @@
 package br.ifrn.tads.arkanoid.jogo;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class CenaDeJogo extends ElementoDaTela {
-
+public class CenaDeJogo extends JPanel {
     private List<Tijolo> tijolos;
     private Raquete raquete;
     private Bola bola;
+    private int m_interval  = 35;    // Milliseconds between updates.
+    private Timer m_timer;           // Timer fires to anmimate one step.
     
     public CenaDeJogo() {
-        super(0, 0, 600, 500);
-        raquete = new Raquete(0, 0);
-        bola = new Bola(0, 0);
+        super();
+        m_timer = new Timer(m_interval, new TimerAction());
+        raquete = new Raquete(this, 0, 0);
+        bola = new Bola(this, 0, 0);
         tijolos = new ArrayList<>();
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 10; j++) {
-                tijolos.add(new Tijolo(1, Tijolo.largura*j, Tijolo.altura*i));
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                tijolos.add(new Tijolo(1, Tijolo.largura * j, Tijolo.altura * i));
             }
         }
-        raquete.y = this.height - raquete.height - 10;
-        bola.x = raquete.x + (raquete.width - bola.width) / 2;
+        raquete.Mover();
+        bola.x = raquete.x + (raquete.width- bola.width) / 2;
         bola.y = raquete.y - bola.height;
+        m_timer.start();
     }
 
     public List<Tijolo> getTijolos() {
@@ -37,16 +45,26 @@ public class CenaDeJogo extends ElementoDaTela {
     public Bola getBola() {
         return bola;
     }
-    
+
     @Override
-    public void Paint(Graphics2D g) {
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g = (Graphics2D) grphcs;
         g.setColor(Color.BLACK);
-        g.fill(this);
-        for(Tijolo t: tijolos) {
+        g.fillRect(0, 0, getWidth(), getHeight());
+        for (Tijolo t : tijolos) {
             t.Paint(g);
         }
+        raquete.Mover();
         raquete.Paint(g);
         bola.Paint(g);
     }
-    
+
+    private class TimerAction implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            bola.Mover();
+            repaint();
+        }
+    }
 }
